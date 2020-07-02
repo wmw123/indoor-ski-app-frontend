56eab4637e1bc6a416e61d92e51567f7fe76b911
wmw123 <wmwaterbolk@gmail.com>
2020-07-02 15:15:13 +0200

    Added Layout to About page

--- pages/about.js
+++ pages/about.js
@@ -1,23 +1,26 @@
 import Link from "next/link";
 import Head from "next/head";
+import Layout from "../components/layout";
 
 export default function About() {
   return (
     <>
-      <Head>
-        <title>About</title>
-      </Head>
-      <h1>About</h1>
-      <h2>
+      <Layout>
+        <Head>
+          <title>About</title>
+        </Head>
+        <h1>About</h1>
+        <h2>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </h2>
         <Link href="/">
-          <a>Back to home</a>
+          <a className="foo" target="_blank" rel="noopener noreferrer">
+            Hello World
+          </a>
         </Link>
-      </h2>
-      <Link href="/">
-        <a className="foo" target="_blank" rel="noopener noreferrer">
-          Hello World
-        </a>
-      </Link>
+      </Layout>
     </>
   );
 }
