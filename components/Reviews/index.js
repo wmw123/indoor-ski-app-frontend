081fcd6706c3e084263396f99cbbb78448df32d3
wmw123 <wmwaterbolk@gmail.com>
2020-07-04 18:34:48 +0200

    Simple stars css added to reviews

--- components/Reviews/index.js
+++ components/Reviews/index.js
@@ -1,5 +1,6 @@
 import fetch from "node-fetch";
 import { useRouter } from "next/router";
+import styles from "./style.module.css";
 
 export default function Reviews(props) {
   //   const router = useRouter();
@@ -10,16 +11,22 @@ export default function Reviews(props) {
   const reviews = props.reviews;
 
   return (
-    <>
+    <div className={styles.container}>
       {reviews.map((review, key) => {
         return (
           <div key={review.id}>
-            <p>{review.rating}</p>
-            <p>{review.rating}</p>
-            <p>{review.quote}</p>
+            <div className="rating">
+              <span>☆</span>
+              <span>☆</span>
+              <span>☆</span>
+              <span>☆</span>
+              <span>☆</span>
+              {review.rating}
+            </div>
+            <p>"{review.quote}"</p>
           </div>
         );
       })}
-    </>
+    </div>
   );
 }
