e975a8fc33d34b737be49bab7022ae37ab14f477
wmw123 <wmwaterbolk@gmail.com>
2020-07-06 12:47:21 +0200

    Sharing buttons for Twitter and email added

--- components/SharingButtons/index.js
+++ components/SharingButtons/index.js
@@ -1,4 +1,11 @@
-import { FacebookShareButton, FacebookIcon } from "react-share";
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  EmailShareButton,
+  EmailIcon,
+} from "react-share";
 
 export default function SharingButtons({ id, name }) {
   return (
@@ -10,6 +17,22 @@ export default function SharingButtons({ id, name }) {
       >
         <FacebookIcon size={32} round />
       </FacebookShareButton>
+      <TwitterShareButton
+        url={`https://www.google.com`}
+        title={`Are you joining me to ${name}? @ [Tag your friends]`}
+      >
+        <TwitterIcon size={32} round />
+      </TwitterShareButton>
+      <EmailShareButton
+        url={`https://www.google.com`}
+        subject={`Are you joining me to ${name}?`}
+        body={`Hi! 
+        Are you joining me to ${name}? 
+        It looks like a super dope indoor ski place! 
+        Check it out -->`}
+      >
+        <EmailIcon size={32} round />
+      </EmailShareButton>
     </div>
   );
 }
