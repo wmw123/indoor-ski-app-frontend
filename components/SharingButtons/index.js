3561046614a5257bbff19ad513122937dc5a5412
wmw123 <wmwaterbolk@gmail.com>
2020-07-06 12:52:09 +0200

    Added simple styling to SharingButtons

--- components/SharingButtons/index.js
+++ components/SharingButtons/index.js
@@ -1,3 +1,5 @@
+import styles from "./style.module.css";
+
 import {
   FacebookShareButton,
   FacebookIcon,
@@ -9,30 +11,36 @@ import {
 
 export default function SharingButtons({ id, name }) {
   return (
-    <div>
-      <FacebookShareButton
-        // update URL once it's live
-        url={`https://www.google.com`}
-        quote={`Are you joining me to ${name}? @ [Tag your friends]`}
-      >
-        <FacebookIcon size={32} round />
-      </FacebookShareButton>
-      <TwitterShareButton
-        url={`https://www.google.com`}
-        title={`Are you joining me to ${name}? @ [Tag your friends]`}
-      >
-        <TwitterIcon size={32} round />
-      </TwitterShareButton>
-      <EmailShareButton
-        url={`https://www.google.com`}
-        subject={`Are you joining me to ${name}?`}
-        body={`Hi! 
+    <div className={styles.container}>
+      <div>
+        <FacebookShareButton
+          // update URL once it's live
+          url={`https://www.google.com`}
+          quote={`Are you joining me to ${name}? @ [Tag your friends]`}
+        >
+          <FacebookIcon size={32} round />
+        </FacebookShareButton>
+      </div>
+      <div>
+        <TwitterShareButton
+          url={`https://www.google.com`}
+          title={`Are you joining me to ${name}? @ [Tag your friends]`}
+        >
+          <TwitterIcon size={32} round />
+        </TwitterShareButton>
+      </div>
+      <div>
+        <EmailShareButton
+          url={`https://www.google.com`}
+          subject={`Are you joining me to ${name}?`}
+          body={`Hi! 
         Are you joining me to ${name}? 
         It looks like a super dope indoor ski place! 
         Check it out -->`}
-      >
-        <EmailIcon size={32} round />
-      </EmailShareButton>
+        >
+          <EmailIcon size={32} round />
+        </EmailShareButton>
+      </div>
     </div>
   );
 }
