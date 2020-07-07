9e9a68742bffb30eff38467ff750a8045ae1da22
wmw123 <wmwaterbolk@gmail.com>
2020-07-07 11:57:38 +0200

    wishlist items displayed on page

--- pages/wishlist.js
+++ pages/wishlist.js
@@ -2,9 +2,19 @@ import Link from "next/link";
 import Head from "next/head";
 import NavBar from "../components/NavBar";
 import Layout from "../components/Layout";
-import AddCount from "../components/AddCount";
+import { useSelector } from "react-redux";
+import { selectWishlist } from "../redux/count/selector";
 
-export default function Wishlist() {
+export default function Wishlist({ indoorskiplaces }) {
+  const wishlistItems = useSelector(selectWishlist);
+
+  console.log("indoorskiplaces", indoorskiplaces.indoorskiplaces);
+  const allIndoorskiplaces = indoorskiplaces.indoorskiplaces;
+
+  // to do: compare indoorskiplaces.id with wishlistItems and make new array
+  // const check = allIndoorskiplaces.filter((id) => id !== wishlistItems);
+  // console.log("check", check);
+
   return (
     <>
       <Head>
@@ -12,6 +22,12 @@ export default function Wishlist() {
       </Head>
       <NavBar />
       <h1>Wishlist</h1>
+      <ul>
+        {wishlistItems.map((wishlistItem, key) => {
+          return <li key={wishlistItem}>{wishlistItem}</li>;
+        })}
+      </ul>
+
       <h2>
         <Link href="/">
           <a>Back to home</a>
@@ -19,4 +35,15 @@ export default function Wishlist() {
       </h2>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`http://localhost:4000/indoorskiplaces`);
+  const indoorskiplaces = await res.json();
+
+  return {
+    props: {
+      indoorskiplaces,
+    },
+  };
 }
